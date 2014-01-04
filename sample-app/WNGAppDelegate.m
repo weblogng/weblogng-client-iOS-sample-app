ea499df85e7eaacff923a9ad05627966958f9f80
Stephen Kuenzli <skuenzli@qualimente.com>
2014-01-03 22:48:50 -0700

    Update WNGLogger library to 0.4.0 and switch to initializing and using the sharedLogger instance.

--- sample-app/WNGAppDelegate.m
+++ sample-app/WNGAppDelegate.m
@@ -12,32 +12,29 @@
 
 @implementation WNGAppDelegate
 
-WNGLogger *logger;
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSString *apiHost = @"ec2-174-129-123-237.compute-1.amazonaws.com:9000";
     //93c5a127-e2a4-42cc-9cc6-cf17fdac8a7f - account 1
     //f37d4458-4fcd-4a4c-93e5-3a5fdd499ed1 - account 9
     NSString *apiKey = @"93c5a127-e2a4-42cc-9cc6-cf17fdac8a7f";
 
-    logger = [[WNGLogger alloc] initWithConfig:apiHost apiKey:apiKey];
+    [WNGLogger initSharedLogger:apiKey];
     
     [self someIntensiveLogic];
     
     return YES;
 }
 
-- (void) someIntensiveLogic {
+- (IBAction) someIntensiveLogic {
     NSString *metricName = @"sample-app-someIntensiveLogic";
-    [logger recordStart:metricName];
+    [[WNGLogger sharedLogger] recordStart:metricName];
     
     int millis_to_sleep = 500 + arc4random_uniform(250);
     float seconds_to_sleep = ((float) millis_to_sleep) / 1000;
     
     [NSThread sleepForTimeInterval:seconds_to_sleep];
     
-    [logger recordFinishAndSendMetric:metricName];
+    [[WNGLogger sharedLogger] recordFinishAndSendMetric:metricName];
 }
 
 @end
